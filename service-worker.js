435cec728659c115b98b3a90bdcc5a66524915e5
Andreas Eberli <andreas@eberli.me>
2023-03-01 17:09:35 +0100

    Zwei neue Tage erfasst

--- service-worker.js
+++ service-worker.js
@@ -5,7 +5,7 @@
  * e.g. Vite or Next.js would hash the files so the name is unique based on content
  * (style-XYZ123.css etc)
  */
-const CACHE_NAME = "mpwa-cache-v12";
+const CACHE_NAME = "mpwa-cache-v13";
 const urlsToCache = [
   "/",
   "/assets/style.css",
